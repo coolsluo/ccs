d54060eb34ed5aee5a08d6174ed521134ea45a9c
linax <linax@knownsec.com>
2026-01-02 23:19:30 +0800

    feat: 添加dashboard页面和后台接口

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,36 @@
 import axios from 'axios';
 import type { AxiosResponse } from 'axios';
-import type { User } from './model';
+import type { User, Lesson } from './model';
+
+
 
 const api = axios.create({
     baseURL: 'http://localhost:3001',
     headers: { 'Content-Type': 'application/json' },
 });
 
-export async function getUsers(): Promise<User[]> {
+export const getUsers = async (): Promise<User[]> => {
     const res: AxiosResponse<User[]> = await api.get(
         '/users'
+    );
+    return res.data;
+}
+
+export const getLessons = async (type?: string,
+    tutor?: string): Promise<Lesson[]> => {
+    const qs = new URLSearchParams();
+    if (type) qs.append('type', type);
+    if (tutor) qs.append('tutor', tutor);
+    const res: AxiosResponse<Lesson[]> = await api.get(
+        `/lessons?${qs.toString()}`
+    );
+    return res.data;
+}
+
+export const updateLesson = async (lesson: Lesson): Promise<Lesson> => {
+    const res: AxiosResponse<Lesson> = await api.patch(
+        `/lessons/${lesson.id}`,
+        lesson
     );
     return res.data;
 }
--- src/api/model.ts
+++ src/api/model.ts
@@ -4,3 +4,28 @@ export interface User {
     password: string,
     role: string,
 }
+
+export const LessonStatus = {
+    Completed: 'Completed',
+    Confirmed: 'Confirmed',
+    Available: 'Available',
+} as const;
+type LessonStatusConst = typeof LessonStatus[keyof typeof LessonStatus];
+
+export const LessonType = {
+    Historic: 'Historic',
+    Upcoming: 'Upcoming',
+    Available: 'Available',
+} as const;
+type LessonTypeConst = typeof LessonType[keyof typeof LessonType];
+
+
+export interface Lesson {
+    id: string,
+    date: string,
+    type: LessonTypeConst,
+    subject: string,
+    students: string[],
+    tutor: string,
+    status: LessonStatusConst,
+}
